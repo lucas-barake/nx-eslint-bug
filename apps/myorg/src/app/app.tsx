0d708d080ccbac8e86898d62cf03c1516de535f4
lucas-barake <86905052+lucas-barake@users.noreply.github.com>
2024-04-05 09:10:06 -0500

    add utils lib

--- apps/myorg/src/app/app.tsx
+++ apps/myorg/src/app/app.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import { utils } from '@myorg/utils';
 
 export const App: React.FC = () => {
   const [message, setMessage] = React.useState<string>("Hello, Nx!");
+
+  const value = utils();
+  value.someField;
 
   return (
     <div>
---
+++ libs/utils/src/lib/utils.ts
@@ -0,0 +1,9 @@
+export type SomeType = {
+  someField: string;
+};
+
+export function utils(): SomeType {
+  return {
+    someField: "someValue",
+  }
+}
